1b332ea9446e39c9fd14669f3dbc39d8acd2739f
Hal <haln_01@proton.me>
2024-04-11 01:32:49 -0700

    bug: encrypt decrypt?

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,4 +1,3 @@
-use base64::{engine::general_purpose::STANDARD_NO_PAD, Engine};
 use cbc::{
     cipher::{block_padding, BlockDecryptMut, BlockEncryptMut, KeyIvInit},
     Decryptor as CBCDecryptor, Encryptor as CBCEncryptor,
@@ -15,16 +14,30 @@ pub type Key = [u8; 32];
 pub const BLOCK_SIZE: usize = 16;
 pub const IV_SIZE: usize = 16;
 
+pub mod encoding {
+    use base64::{
+        engine::general_purpose::{self, STANDARD_NO_PAD},
+        Engine,
+    };
+    const BASE64_ENCODING: general_purpose::GeneralPurpose = STANDARD_NO_PAD;
+
+    pub struct Base64;
+    impl Base64 {
+        pub fn encode(buf: &[u8]) -> String {
+            return BASE64_ENCODING.encode(buf);
+        }
+
+        pub fn decode(buf: &[u8]) -> Result<Vec<u8>, base64::DecodeError> {
+            return BASE64_ENCODING.decode(buf);
+        }
+    }
+}
 pub struct Encoding;
 impl Encoding {
     pub fn sha256(buf: &mut [u8; 32], data: &[u8]) {
         let mut sha256 = sha2::Sha256::new();
         sha256.input(data);
         sha256.result(buf);
-    }
-
-    pub fn base64(buf: &[u8]) -> String {
-        return STANDARD_NO_PAD.encode(&buf);
     }
 }
 
@@ -35,7 +48,7 @@ impl<'a> Encryptor {
         return Encryptor(key);
     }
     /// returns the overhead (padding length) of plaintext with `pt_len`
-    pub fn overhead(&self, pt_len: usize) -> usize {
+    pub fn pad_len(&self, pt_len: usize) -> usize {
         let remainder = pt_len % BLOCK_SIZE;
         return match remainder == 0 {
             true => BLOCK_SIZE,
@@ -48,21 +61,28 @@ impl<'a> Encryptor {
     /// if `ciphertext.len() % 16 == 0`, then `buf.len() = ciphertext.len() + 16`
     /// pad it to a multiple of 16 otherwise.
     pub fn encrypt(self, plaintext: &'a [u8], buf: &'a mut [u8]) -> Result<(), Box<dyn Error>> {
+        dbg!(plaintext.len());
+        dbg!(buf.len());
         // make `iv`, then copy it into the first `IV_SIZE` bytes of `buf`
-        let mut iv: Vec<u8> = make_buffer(IV_SIZE);
-        OsRng.try_fill_bytes(&mut iv)?;
-        iv.iter().enumerate().for_each(|(i, &bytes)| buf[i] = bytes);
-
+        OsRng.try_fill_bytes(&mut buf[..IV_SIZE])?;
         // cipher `plaintext` and write to `buf`
-        SerpentCbcEnc::new_from_slices(&self.0, &iv)?
+        SerpentCbcEnc::new_from_slices(&self.0, &buf[..IV_SIZE])?
             .encrypt_padded_b2b_mut::<block_padding::Pkcs7>(plaintext, &mut buf[IV_SIZE..])?;
 
         Ok(())
     }
-}
 
-pub struct Decryptor {}
-impl Decryptor {}
+    pub fn decrypt(self, ciphertext: &'a [u8]) -> Result<Vec<u8>, Box<dyn Error>> {
+        dbg!(ciphertext.len());
+        let nonce = &ciphertext[..IV_SIZE];
+        let ciphertext = &ciphertext[IV_SIZE..];
+        dbg!(ciphertext.len());
+        let mut buf: Vec<u8> = Vec::with_capacity(ciphertext.len());
+        SerpentCbcDec::new_from_slices(&self.0, nonce)?
+            .decrypt_padded_b2b_mut::<block_padding::Pkcs7>(ciphertext, &mut buf)?;
+        return Ok(buf.to_owned());
+    }
+}
 
 pub fn make_buffer(size: usize) -> Vec<u8> {
     let mut buf: Vec<u8> = Vec::with_capacity(size);
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use clap::Parser;
 use std::{
     fs,
-    io::{self, BufRead, Read, Write},
+    io::{self, Read, Write},
     path::Path,
     process::exit,
 };
@@ -9,51 +9,66 @@ use std::{
 mod cli;
 mod crypto;
 
-trait HandleFile {
-    fn handle(self, cli: &cli::CLI);
-}
-
 fn main() {
     let cli = cli::CLI::parse();
     match cli.command {
-        cli::Command::Open => open_file(cli),
-        cli::Command::Seal => seal_file(cli),
+        cli::Command::Open => open(cli),
+        cli::Command::Seal => seal(cli),
     };
 }
 
-fn open_file(cli: cli::CLI) {}
+fn open(cli: cli::CLI) {
+    // input file handler
+    let input_file = Path::new(&cli.file_name);
+    let ciphertext = fs::read(&input_file).unwrap_or_else(|err| {
+        eprintln!("Failed to read input file:\n{err}");
+        exit(1);
+    });
 
-fn seal_file(cli: cli::CLI) {
-    // read input file
+    let ciphertext = crypto::encoding::Base64::decode(&ciphertext).unwrap_or_else(|err| {
+        eprintln!("Failed to decode input file:\n{}", err);
+        exit(1);
+    });
+
+    // output file handler
+    let mut output_file = make_file_out(&cli.out).unwrap_or_else(|err| {
+        eprintln!("Failed to open output file {}:\n{}", &cli.out, err);
+        exit(1);
+    });
+
+    // read encryption key
+    let password = rpassword::prompt_password("Enter encryption key: ").unwrap_or_else(|err| {
+        eprintln!("Failed to read encryption key:\n{}", err);
+        exit(1);
+    });
+
+    // make cipher block
+    let mut key: crypto::Key = [0; 32];
+    crypto::Encoding::sha256(&mut key, password.as_bytes());
+    let block = crypto::Encryptor::new(key);
+    let plaintext = block.decrypt(&ciphertext).unwrap_or_else(|err| {
+        eprintln!("Failed to decrypt data:\n{}", err);
+        exit(1);
+    });
+
+    println!("{}", std::str::from_utf8(&plaintext).unwrap());
+}
+
+fn seal(cli: cli::CLI) {
+    // input file handler
     let input_file = Path::new(&cli.file_name);
     let plaintext = fs::read(&input_file).unwrap_or_else(|err| {
         eprintln!("Failed to read input file:\n{err}");
         exit(1);
     });
 
-    // open output file
-    let path = std::path::Path::new(&cli.out);
-    let mut output_file_opt = fs::OpenOptions::new();
+    // let plaintext = crypto::Encoding::b
 
-    if path.exists() {
-        let mut overwrite: [u8; 1] = [0];
-        print!("{} found. Overwrite? [y/n]: ", &cli.out);
-        io::stdout().flush().unwrap();
-        let stdin = io::stdin();
-        stdin.lock().read(&mut overwrite).unwrap();
-
-        match overwrite == *b"y" {
-            true => output_file_opt.truncate(true),
-            false => {
-                eprintln!("Ouput file exists.");
-                exit(1);
-            }
-        };
-    } else {
-        output_file_opt.create_new(true);
-    }
-
-    let mut output_file = output_file_opt.write(true).open(&path).unwrap();
+    // output file handler
+    let mut output_file = make_file_out(&cli.out).unwrap_or_else(|err| {
+        eprintln!("Failed to open output file {}:\n{}", &cli.out, err);
+        exit(1);
+    });
 
     // read encryption key
     let password = rpassword::prompt_password("Enter encryption key: ").unwrap_or_else(|err| {
@@ -79,7 +94,7 @@ fn seal_file(cli: cli::CLI) {
 
     // encrypt data
     let pt_len = plaintext.len();
-    let size = pt_len + block.overhead(pt_len) + crypto::IV_SIZE;
+    let size = pt_len + block.pad_len(pt_len) + crypto::IV_SIZE;
     let mut ciphertext: Vec<u8> = crypto::make_buffer(size);
     block
         .encrypt(plaintext.as_slice(), &mut ciphertext)
@@ -88,8 +103,8 @@ fn seal_file(cli: cli::CLI) {
             exit(1);
         });
 
-    // write to output file
-    let c = crypto::Encoding::base64(ciphertext.as_slice());
+    // write out the bytes
+    let c = crypto::encoding::Base64::encode(ciphertext.as_slice());
     match output_file.write(c.as_bytes()) {
         Err(err) => {
             eprintln!("Failed to write to output file:\n{}", err)
@@ -101,4 +116,30 @@ fn seal_file(cli: cli::CLI) {
             )
         }
     };
+}
+
+fn make_file_out(file_name: &str) -> io::Result<fs::File> {
+    // open output file
+    let path = std::path::Path::new(file_name);
+    let mut output_file_opt = fs::OpenOptions::new();
+
+    if path.exists() {
+        let mut overwrite: [u8; 1] = [0];
+        print!("{} found. Overwrite? [y/n]: ", file_name);
+        io::stdout().flush()?;
+
+        let stdin = io::stdin();
+        stdin.lock().read(&mut overwrite)?;
+        match overwrite[0] == b'y' {
+            true => output_file_opt.truncate(true),
+            false => {
+                eprintln!("Ouput file exists.");
+                exit(1);
+            }
+        };
+    } else {
+        output_file_opt.create_new(true);
+    }
+
+    return output_file_opt.write(true).open(&path);
 }
