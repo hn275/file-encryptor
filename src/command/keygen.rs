fb8a46e831310e89f0cc1774429d7e0a2b1385fc
Hal <haln_01@proton.me>
2024-04-16 23:17:28 -0700

    fix: removed base64 encoding

--- src/command/keygen.rs
+++ src/command/keygen.rs
@@ -2,39 +2,22 @@ use super::Command;
 use crate::crypto::encoding;
 use aes_gcm::aead::{rand_core::RngCore, OsRng};
 use clap::Parser;
-use std::{
-    default,
-    io::{self, Write},
-};
+use std::io::{self, Write};
 
 #[derive(Parser, Debug, Clone)]
 pub struct KeyGen {
     /// password
     #[arg(short, long)]
     password: Option<String>,
-
-    ///
-    #[arg(short, long)]
-    base64: bool,
 }
 
 impl Command for KeyGen {
     fn handle(&self) -> Result<(), io::Error> {
-        let mut key_buf: [u8; 32] = [0; 32];
+        let mut buf: [u8; 32] = [0; 32];
         match &self.password {
-            None => OsRng.fill_bytes(&mut key_buf),
-            Some(password) => encoding::sha256::encode(&mut key_buf, password.as_bytes()),
+            None => OsRng.fill_bytes(&mut buf),
+            Some(password) => encoding::sha256::encode(&mut buf, password.as_bytes()),
         };
-        if !self.base64 {
-            io::stdout().lock().write_all(&key_buf)?;
-            return Ok(());
-        }
-
-        let mut buf = [0 as u8; encoding::base64::ENCODED_LEN_32];
-        encoding::base64::encode(&mut buf, &key_buf).map_err(|err| {
-            dbg!(err);
-            io::Error::new(io::ErrorKind::Other, "buffer to small")
-        })?;
         io::stdout().lock().write_all(&buf)?;
         Ok(())
     }
--- src/crypto/encoding.rs
+++ src/crypto/encoding.rs
@@ -1,19 +1,3 @@
-pub mod base64 {
-
-    use base64::{self, engine::general_purpose::STANDARD_NO_PAD as b64, Engine};
-
-    /// base64 encoded length from 32 bytes buffer
-    pub const ENCODED_LEN_32: usize = 43;
-
-    pub fn encode(
-        buf: &mut [u8; ENCODED_LEN_32],
-        msg: &[u8],
-    ) -> Result<(), base64::EncodeSliceError> {
-        b64.encode_slice(msg, buf)?;
-        Ok(())
-    }
-}
-
 pub mod sha256 {
     use crypto::{self, digest::Digest as _, sha2};
 
