8a5350c65ce3cab68f27680f2bf70433944a3c19
Hal <haln_01@proton.me>
2024-08-13 16:09:08 -0700

    feat(keygen)!: parallelized keygen

--- src/command/keygen.rs
+++ src/command/keygen.rs
@@ -1,35 +1,37 @@
 use crate::{crypto::cipher, error, Command};
 use clap::Parser;
 use crypto::scrypt;
-use std::io::{self, Read, Write};
+use std::{
+    io::{self, Read, Write},
+    sync::{Arc, Mutex},
+};
 
 type Key = [u8; cipher::KEY_LEN];
 
-struct Scrypt {
-    param: scrypt::ScryptParams,
+struct Engine {
+    scrypt: scrypt::ScryptParams,
+    key_buf: Key,
 }
 
-impl Scrypt {
+impl Engine {
     pub fn new() -> Self {
         // https://tobtu.com/minimum-password-settings/
-        let param = scrypt::ScryptParams::new(16, 8, 2);
-        Scrypt { param }
+        let scrypt = scrypt::ScryptParams::new(16, 8, 2);
+        let key_buf: Key = Default::default();
+        Engine { scrypt, key_buf }
     }
 
-    pub fn gen(&self, password: &[u8], key: &mut Key) -> error::Result<()> {
-        if password.len() < 8 {
-            return Err(error::Error::IO(String::from("Password too short")));
-        }
-
+    pub fn update(&mut self, password: &[u8]) {
         let mut sub_key: Key = Default::default();
-        scrypt::scrypt(password, &[], &self.param, &mut sub_key);
-
-        Self::xor_key(key, &sub_key);
-
-        Ok(())
+        scrypt::scrypt(password, &[], &self.scrypt, &mut sub_key);
+        Self::xor_key(&mut self.key_buf, &sub_key);
     }
 
-    pub fn xor_key(key_buf: &mut Key, sub_key: &Key) {
+    pub fn bytes(&self) -> &[u8] {
+        self.key_buf.as_ref()
+    }
+
+    fn xor_key(key_buf: &mut Key, sub_key: &Key) {
         for i in 0..cipher::KEY_LEN {
             key_buf[i] ^= sub_key[i];
         }
@@ -45,27 +47,47 @@ pub struct KeyGen {
 
 impl Command for KeyGen {
     fn handle(&self) -> error::Result<()> {
-        const MAX_KEY_SIZE: usize = 0xff;
-        let mut key_buf: Key = Default::default();
+        let keygen = Arc::new(Mutex::new(Engine::new()));
 
         if let Some(pw) = &self.password {
-            let scrypt = Scrypt::new();
-            scrypt.gen(pw.as_bytes(), &mut key_buf)?;
+            keygen
+                .lock()
+                .expect("unable to obtain thread lock for key generation engine")
+                .update(pw.as_bytes());
         } else {
-            let scrypt = Scrypt::new();
+            let scope: error::Result<()> = rayon::scope(|s| {
+                let mut stdin = io::stdin().lock();
+                const MAX_KEY_SIZE: usize = 0xffff;
 
-            let mut buf: [u8; MAX_KEY_SIZE] = [0_u8; MAX_KEY_SIZE];
-            let mut stdin = io::stdin().lock();
+                loop {
+                    let mut buf = [0_u8; MAX_KEY_SIZE];
+                    let bytes_read = stdin.read(&mut buf)?;
 
-            loop {
-                let bytes_read = stdin.read(&mut buf)?;
-                scrypt.gen(buf.as_slice(), &mut key_buf)?;
-                if bytes_read < MAX_KEY_SIZE {
-                    break;
+                    let keygen = Arc::clone(&keygen);
+                    s.spawn(move |_| {
+                        keygen
+                            .lock()
+                            .expect("unable to obtain thread lock for key generation engine")
+                            .update(buf.as_slice());
+                    });
+
+                    if bytes_read < MAX_KEY_SIZE {
+                        break;
+                    }
                 }
-            }
+
+                Ok(())
+            });
+
+            scope?;
+
+            let keygen = keygen
+                .lock()
+                .expect("unable to obtain thread lock for key generation engine");
+
+            io::stdout().lock().write_all(keygen.bytes())?;
         }
 
-        Ok(io::stdout().lock().write_all(&key_buf)?)
+        Ok(())
     }
 }
